a36ead2a4e74f95f92ac89ac09cf0199016e37ef
brenohsilva <brennohsilva@hotmail.com>
2025-07-12 17:52:55 -0300

    feat: create url resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { UrlsModule } from './urls/urls.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, UrlsModule],
   controllers: [],
   providers: [],
 })
