10dcffcfc2642498effaaff53b2d883d2adf62f8
brenohsilva <brennohsilva@hotmail.com>
2025-07-13 17:55:35 -0300

    chore: enable cors to specific origins whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ForbiddenException, ValidationPipe } from '@nestjs/common';
 import { UserNotFoundErrorFilter } from './filters/not-found-error/not-found-error.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS;
+  if (allowedOrigins) {
+    const whitelist = allowedOrigins.split(',').map((item) => item.trim());
+    app.enableCors({
+      origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new ForbiddenException('origin not allowed by CORS'));
+        }
+      },
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    });
+  } else {
+    console.warn(
+      'CORS_ALLOWED_ORIGINS não está definido. CORS não está habilitado.',
+    );
+  }
 
   app.useGlobalFilters(new UserNotFoundErrorFilter());
 
@@ -14,6 +36,8 @@ async function bootstrap() {
     new ValidationPipe({
       errorHttpStatusCode: 422,
       transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     }),
   );
 
