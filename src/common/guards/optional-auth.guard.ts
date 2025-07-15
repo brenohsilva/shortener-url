8f3e3e46df6f1df906399218221b8ba51c771611
brenohsilva <brennohsilva@hotmail.com>
2025-07-15 11:37:56 -0300

    fix: fix bug in auth guard

--- src/common/guards/optional-auth.guard.ts
+++ src/common/guards/optional-auth.guard.ts
@@ -1,19 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class OptionalAuthGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext) {
-    return super.canActivate(context);
-  }
-
   handleRequest(
     err: any,
     user: any,
     info: any,
     context: ExecutionContext,
   ): any {
+    if (
+      info?.name === 'JsonWebTokenError' ||
+      info?.name === 'TokenExpiredError'
+    ) {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
     return user || null;
   }
 }
